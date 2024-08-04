a6aa8183a50d2dafb06f1cd295b6d82ffa9da68e
Timur Galyautdinov <galy.tim@gmail.com>
2024-08-04 21:31:28 +0300

    add define in IStudentService

--- Application/Services/StudentsService/Interfaces/IStudentsService.cs
+++ Application/Services/StudentsService/Interfaces/IStudentsService.cs
@@ -41,5 +41,13 @@ namespace Webflow.Application.Services.StudentsService.Interfaces
         /// <param name="cancellationToken">Токен отмены операции</param>
         /// <returns>Ответ, содержащий результат операции удаления</returns>
         public Task<BaseResponse<string>> SoftDeleteStudent(Guid? id, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Создание студента по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор студента</param>
+        /// <param name="cancellationToken">Токен отмены операции</param>
+        /// <returns>Ответ, содержащий результат операции удаления</returns>
+        public Task<BaseResponse<string>> CreateStudent(CreateStudentRequest request, CancellationToken cancellationToken);
     }
 }
