631c056fd517328ac09e1e527ce28904bb687e83
Timur Galyautdinov <galy.tim@gmail.com>
2024-08-04 18:16:09 +0300

    add documentation in endpoint delete in StudentsController

--- API/Controllers/Students/StudentsController.DeleteStudent.cs
+++ API/Controllers/Students/StudentsController.DeleteStudent.cs
@@ -5,7 +5,14 @@ namespace Webflow.API.Controllers.Students
 {
     public partial class StudentsController : ControllerBase
     {
-
+        /// <summary>
+        /// Удаляет студента по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор студента</param>
+        /// <param name="cancellationToken">Токен для отмены операции</param>
+        /// <returns>Ответ, содержащий результат операции удаления</returns>
+        /// <response code="200">Успешный ответ с результатом удаления</response>
+        /// <response code="400">Ошибка при обработке запроса, например, неверный идентификатор</response>
         [HttpDelete("{id}")]
         public async Task<ActionResult<BaseResponse<string>>> DeleteStudent(Guid? id, CancellationToken cancellationToken) 
         {
