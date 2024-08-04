a4829bcbe6da1ba66477abb4fb0cffde444f9173
Galyautdinov Timur <55600825+galytim@users.noreply.github.com>
2024-08-04 19:27:45 +0300

    add Soft delete (#4)

---
+++ API/Controllers/Students/StudentsController.SoftDeleteStudent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Webflow.API.Dto.Shared;
+
+namespace Webflow.API.Controllers.Students
+{
+    public partial class StudentsController : ControllerBase
+    {
+        /// <summary>
+        /// Удаляет студента по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор студента</param>
+        /// <param name="cancellationToken">Токен для отмены операции</param>
+        /// <returns>Ответ, содержащий результат операции удаления</returns>
+        /// <response code="200">Успешный ответ с результатом удаления</response>
+        /// <response code="400">Ошибка при обработке запроса, например, неверный идентификатор</response>
+        
+        [HttpDelete("soft-delete/{id}")]
+        public async Task<ActionResult<BaseResponse<string>>> SoftDeleteStudent(Guid? id, CancellationToken cancellationToken) 
+        {
+            var result = await studentsService.SoftDeleteStudent(id, cancellationToken);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Application/Services/StudentsService/Implementations/StudentsService.SoftDeleteStudent.cs
@@ -0,0 +1,46 @@
+﻿using Webflow.API.Dto.Shared;
+using Webflow.Application.Messages.ErrorMessages.Students;
+using Webflow.Application.Messages.SuccessefulMessages.Students;
+using Webflow.Application.Services.StudentsService.Interfaces;
+
+namespace Webflow.Application.Services.StudentsService.Implementations
+{
+    public partial class StudentsService : IStudentsService
+    {
+        public async Task<BaseResponse<string>> SoftDeleteStudent(Guid? id, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<string>() {
+                IsSuccess = false,
+                ErrorMessages = new List<string>()
+            };
+
+            if (id == null)
+            {
+                response.ErrorMessages.Append(StudentErrorMessages.ID_CANNOT_BE_NULL);
+                return response;
+            }
+
+            var student = await studentsRepository.GetByIdAsync((Guid)id, cancellationToken);
+
+            if (student == null)
+            {
+                response.ErrorMessages.Append(StudentErrorMessages.STUDENT_NOT_FOUND);
+                return response;    
+            }
+            student.RemovedAt = DateTime.UtcNow;
+
+            var result = await studentsRepository.UpdateAsync(student, cancellationToken);
+
+            if (result == null)
+            {
+                response.ErrorMessages.Append(StudentErrorMessages.STUDENT_CANNOT_DELETE);
+                return response;
+            }
+
+            response.IsSuccess = true;
+            response.Data = StudentSuccessfulMessages.STUDENT_DELETED;
+            return response;
+        }
+    }
+}
+   
