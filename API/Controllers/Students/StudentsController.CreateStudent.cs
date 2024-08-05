917c47d52ebd8382b42db3a4d352ec334d33f1c4
Timur Galyautdinov <galy.tim@gmail.com>
2024-08-06 02:27:50 +0300

    useless using

---
+++ API/Controllers/Students/StudentsController.CreateStudent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Webflow.API.Dto.Shared;
+using Webflow.API.Dto.Students;
+
+namespace Webflow.API.Controllers.Students
+{
+    public partial class StudentsController : ControllerBase
+    {
+        /// <summary>
+        /// Создает нового студента
+        /// </summary>
+        /// <param name="request">Запрос с данными для создания студента</param>
+        /// <param name="cancellationToken">Токен для отмены операции</param>
+        /// <returns>Ответ, содержащий результат операции создания студента</returns>
+        /// <response code="200">Успешный ответ с результатом создания студента</response>
+        /// <response code="400">Ошибка при обработке запроса, например, неверные данные</response>
+        [HttpPost]
+        public async Task<ActionResult<BaseResponse<string>>> CreateStudent(CreateStudentRequest request, CancellationToken cancellationToken)
+        {
+            var result = await studentsService.CreateStudent(request, cancellationToken);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Application/Services/StudentsService/Implementations/StudentsService.CreateStudent.cs
@@ -0,0 +1,33 @@
+﻿using Webflow.API.Dto.Shared;
+using Webflow.API.Dto.Students;
+using Webflow.Application.Messages.ErrorMessages.Students;
+using Webflow.Application.Services.StudentsService.Interfaces;
+using Webflow.Domain.Students;
+
+namespace Webflow.Application.Services.StudentsService.Implementations
+{
+    public partial class StudentsService : IStudentsService
+    {
+        public async Task<BaseResponse<StudentViewDto>> CreateStudent(CreateStudentRequest request, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<StudentViewDto>()
+            {
+                IsSuccess = false,
+                ErrorMessages = new List<string>()
+            };
+            var student = mapper.Map<Student>(request);
+
+            var result = await studentsRepository.AddAsync(student, cancellationToken);
+
+            if (result == null)
+            {
+                response.ErrorMessages.Append(StudentErrorMessages.STUDENT_CANNOT_CREATE);
+                return response;
+            }
+
+            response.IsSuccess = true;
+            response.Data = mapper.Map<StudentViewDto>(student);
+            return response;
+        }
+    }
+}
