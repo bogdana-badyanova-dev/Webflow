acc5d6f56a04149482a8025e8266e66a776f9191
Timur Galyautdinov <galy.tim@gmail.com>
2024-08-06 02:08:50 +0300

    add update request for student

---
+++ API/Dto/Students/UpdateStudentRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Webflow.API.Dto.Students
+{
+    /// <summary>
+    /// Запрос для Обновления данных о студенте
+    /// </summary>
+    public class UpdateStudentRequest : BaseStudentRequest
+    {
+
+    }
+}
