10feca0ee72e2411481af5c000611785c53fa753
Timur Galyautdinov <galy.tim@gmail.com>
2024-08-06 02:07:57 +0300

    add create request for student

---
+++ API/Dto/Students/CreateStudentRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Webflow.API.Dto.Students
+{
+    /// <summary>
+    /// Запрос для создания студента
+    /// </summary>
+    public class CreateStudentRequest : BaseStudentRequest
+    {
+        /// <summary>
+        /// Имя студента
+        /// </summary>
+        [DefaultValue("Sasha")]
+        new public required string FirstName { get; set; }
+        /// <summary>
+        /// Фамилия студента
+        /// </summary>
+        [DefaultValue("Gray")]
+        new public required string LastName { get; set; }
+        /// <summary>
+        /// Электронная почта студента
+        /// </summary>
+        [DefaultValue("example@gmail.com")]
+        new public required string Email { get; set; }
+
+    }
+}
