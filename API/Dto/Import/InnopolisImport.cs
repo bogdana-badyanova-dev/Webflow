6532203fe60b1b5f981d5200cc61d80383a44c23
Timur Galyautdinov <galy.tim@gmail.com>
2024-08-08 21:07:21 +0300

    Add models BaseImportDto, InnopolisImport, MoodleImport

---
+++ API/Dto/Import/InnopolisImport.cs
@@ -0,0 +1,23 @@
+﻿using Webflow.Application.Enums;
+
+namespace Webflow.API.Dto.Import
+{
+    public class InnopolisImport : BaseImportDto
+    {
+        public string InnopolisAccountId { get; set; }
+        public string Snils { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public ActivityStatusEnum ActivityStatus { get; set; }
+        public int AssessmentStage { get; set; }
+        public AssessmentStatusEnum AssessmentStatus { get; set; }
+        public DateTime AssessmentCompleteDate { get; set; }
+        public int AttemptCount { get; set; }
+        public int TryTime { get; set; }
+        public string SkillName { get; set; }
+        public SkillLevelEnum PlannedSkillLevel { get; set; }
+        public SkillLevelEnum FinalSkillLevel { get; set; }
+        public ProficiencyLevelEnum ProficiencyLevel { get; set; }
+        public double FinalScores { get; set; }
+    }
+}
---
+++ Infrastructure/Migrations/20240808180240_RenameColumnDeadlineToCompleteDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webflow.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumnDeadlineToCompleteDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Deadline",
+                table: "MoodleCourseElements",
+                newName: "CompleteDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CompleteDate",
+                table: "MoodleCourseElements",
+                newName: "Deadline");
+        }
+    }
+}
